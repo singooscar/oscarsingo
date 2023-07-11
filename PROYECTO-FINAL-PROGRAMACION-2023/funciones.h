cf952bde4855ddc2738d51dfe7a66862b9cf328d
singooscar <137453325+singooscar@users.noreply.github.com>
2023-07-11 13:39:17 -0500

    Add files via upload

---
+++ PROYECTO-FINAL-PROGRAMACION-2023/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <stdio.h>
+
+void verProductos(FILE *archivo);
+void ingresarProducto(FILE *archivo);
+void modificarProducto(FILE *archivo);
+void eliminarProducto(FILE *archivo);
+
+#endif /* FUNCIONES_H */
